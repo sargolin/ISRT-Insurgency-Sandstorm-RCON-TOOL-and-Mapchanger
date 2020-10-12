9cf934b42cf7dd6250b3ccbb31e561a7c68ed429
O. Edelmeier <70414281+sargolin@users.noreply.github.com>
2020-10-12 13:17:19 +0200

    basic setup

---
+++ windows_isrt/isrt_main.py
@@ -0,0 +1,14 @@
+#ISRT - Insurgency Sandstorm RCon Tool; 12.10.2020, Sargolin aka @ Madman
+#In case of questions: oe@edelmeier.org
+#Git: https://github.com/sargolin/ISRT-Insurgency-Sandstorm-RCON-TOOL-and-Mapchanger.git
+#v0.1 -Creation of basic structure plus database file
+#Database: ./db/isrt_data.db
+#Main-program: This File
+#Imported classes/functions/libraries:
+
+#Importing required classes and libraries
+import PyQt5, sys, os
+
+
+
+
