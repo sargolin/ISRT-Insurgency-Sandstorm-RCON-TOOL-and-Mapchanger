757ab9385cbe3ccdff9344e73eb6269ae54b779f
O. Edelmeier <70414281+sargolin@users.noreply.github.com>
2020-12-30 14:51:58 +0100

    Added 4 new maps

--- Source_Code/bin/isrt_gui.py
+++ Source_Code/bin/isrt_gui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '.\source_files\isrt_gui_v0.7.1.ui'
+# Form implementation generated from reading ui file '.\source_files\isrt_gui.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.1
 #
